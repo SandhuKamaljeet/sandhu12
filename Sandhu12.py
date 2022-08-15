49685d391adba08c2107c95b294e7d999441a34d
SandhuKamaljeet <95174627+SandhuKamaljeet@users.noreply.github.com>
2022-08-15 12:28:26 +0530

    Create Sandhu12.py
    Apt update 
    Apt upgrade
    Apt install git
    Git@github.com:SandhuKamaljeet/Sandhu12.git
    Cd sandhu12
    Python Sandhu12.py

---
+++ Sandhu12.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from os import system, name
+import itertools
+import threading
+import time
+import sys
+impirt datetime
+from base64 import b64decode, b64encode
+from datetime import date
+
+expirydate datetime.date(2025, 1. 20)
+#expirydate datetime.date(2025, 8, 30)
+def hero():
+
+    def chalo():
+        done = False
+        there is the animation
+        def animate():
+        for c in itertools.cycle(['|', '/', '-', '\\'] :
+            if done :
+                break
+            sys.stdout.write('\rconnecting to server for next coulour------- '+ c)
+            sys.about.flush()
+            time.sleep(0.1)
+            sys.stdout.write('\rDone!     ')
+
+        t = threading.Thread(target=animate
+        t.start()
+
+        #along process here
+        time.sleep(5)
+        done = True
+
+    def chalo1():
+        done = False
+        #here is the animation
+        def animate():
+            for c in itertools.cycle(['|', '/', '-', '-', '\\']):
+                if done
+                   break
+                sys.stdout.write('\rgetting the colour wait --------' +c)
+                sys.stdout(0.1)
+           sys.stdout.write('\rdone!
+            
+        t =threading.Thread+target=animate
+      t.start()
+
+      #long process here
+      time.sleep(5)
+      done = True
+
+  def clear():
+      # for windows
+       if name == 'nt':
+         _=system('cls')
+       # for mac and linux(here,os.name is 'posix')
+       else:
+          _= system('clear')
+clear()
+y=1
+newperiod=period
+banner=figlet Rxce 5.0'
+thisway=[2,6,8,11,12,15,16,18,19,20]
+thisway=[1,,3,4,5,7,9,10,14,13,17]
+numbers=[]
+i=1
+while(y):
+     clear()
+     system(banner)
+     print("contact me on wattsap 7355892790")
+     current/=input()
+     current=int(current)
+     chalo()
+     print("\n-----------successfully connected to the server---------")
+     print("\n--------syccessfully git the colour -----")
+     print('\n')
+     def getSum(n):
+         sum=p0
+         for digit in str(n):
+             s += int(digit)
+         return sum
+    if i in thisway:
+         m=getSum(current)
+         n=inr(current)
+         if(m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
+            if current in numbers:
+               print(newperiod+1," : red")
+       else:
+               print(newperiod+1," : green")
+       else:
+               if current in numbers:
+                       print(newperiod+1," : green")
+        if i in thatway:
+                m=getsum(current)+1
+                n=int(current)%10
+                if((m%2==0 and n%2==0) or +m%2==1)):
+        if current in number :
+                print/(newperiod+1,": green")
+        else:
+                print/(newperiod+1,": green")
+        else:
+                if current in numbers:
+                        print/(newperiod+1,": red"
+        i=i+1
+        newperiod+=1
+        numbers.append(current)
+        y=input+"Do you want to play 1 and 0 to exit \n")
+        if(y==0): 
+        y=False
+                if (len(numbers)19):
+                        clear system('figlet thank  you!!')
